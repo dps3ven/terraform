3085672f789b1c97234de925463d33a159aead2c
dan peterson <dpitpro@gmail.com>
2021-06-18 01:42:46 -0500

    fixed naming

--- aws/rds/database.tf
+++ aws/rds/database.tf
@@ -2,9 +2,9 @@ resource "aws_db_instance" "default" {
   allocated_storage    = 10
   engine               = "mysql"
   engine_version       = "5.7"
-  identifier           = "menu-db-instance"
+  identifier           = "your-db-instance"
   instance_class       = "db.t3.micro"
-  name                 = "menu_items"
+  name                 = "your_db"
   username             = "foo"
   password             = "foobarbaz"
   parameter_group_name = "default.mysql5.7"
