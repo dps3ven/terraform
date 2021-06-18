a1f687910873052bb7fd3d4eae2e60eb8faa1fe2
dan peterson <dpitpro@gmail.com>
2021-06-18 01:50:37 -0500

    added resources

--- aws/rds/database.tf
+++ aws/rds/database.tf
@@ -1,17 +1,17 @@
 resource "aws_db_instance" "default" {
-  allocated_storage    = 10
-  engine               = "mysql"
-  engine_version       = "5.7"
-  identifier           = "your-db-instance"
-  instance_class       = "db.t3.micro"
-  name                 = "your_db"
-  username             = "foo"
-  password             = "foobarbaz"
-  parameter_group_name = "default.mysql5.7"
-  publicly_accessible  = true
-  skip_final_snapshot  = true
+  allocated_storage      = 10
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  identifier             = "your-db-instance"
+  instance_class         = "db.t3.micro"
+  name                   = "your_db"
+  username               = "foo"
+  password               = "foobarbaz"
+  parameter_group_name   = "default.mysql5.7"
+  publicly_accessible    = true
+  skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.sg.id]
-  db_subnet_group_name = aws_db_subnet_group.default.id
+  db_subnet_group_name   = aws_db_subnet_group.default.id
 }
 
 ## availability zone thing?
